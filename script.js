7b5d27b5140ef08002b45b8d12b6ac85f2623cda
Booker <45610604+Booker-M@users.noreply.github.com>
2020-03-24 22:18:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //VARIABLES
 
-let newGame = false; //true
+let newGame = true; //true
 let gameStart, deathTime, waveTime, currentTime;
 let bounds;
 const types = { STAR: 'Star', SHIP: 'Ship', PLANET: 'Planet', BULLET: 'Bullet', DEBRIS: 'Debris', LOOT: 'Loot', Effect: 'Effect'};
